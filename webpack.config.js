a8c6e7d9bff9a1f676fea38957805b8133af5812
jmatos01 <jmatos@techlaunch.io>
2018-09-04 19:41:06 -0400

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     compress: true,
-    port: 4500
+    port: 8500
   },
   plugins: [
     new UglifyJsPlugin()
